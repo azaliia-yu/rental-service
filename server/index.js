f5a4efb4af72a15ee0d7b480a326fe473c66fa54
azaliia-yu <kk9579004@gmail.com>
2026-02-17 18:14:40 +0300

    Реализован основной функционал серверной части: - централизованная обработка ошибок (ApiError, middleware); - загрузка файлов через multer и раздача статики; - регистрация пользователя с аватаром, хеширование пароля; - создание предложений по аренде с загрузкой превью и фотографий; - адаптация списка предложений для клиента (полные URL изображений); - получение полного предложения с данными автора; - наполнение базы тестовыми отзывами.

--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ import sequelize from './config/database.js';
 //import './models/offer.js';
 //import './models/review.js';
 import cors from 'cors';
+import path from 'path'
 import { router } from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const ___filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(___filename)
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,7 +21,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+
 app.use('/', router);
+app.use(errorMiddleware);
 
 app.get('/', (req,res)=>{
     res.status(200).json({message:'Ура! ВСЕ заработало'})
